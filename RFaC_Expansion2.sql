80194a04da972f4d0a1ac78837eba0cb9eb2564c
ActualSailorCat <52355344+ActualSailorCat@users.noreply.github.com>
2020-05-22 21:34:49 -0400

    Initial commit

---
+++ RFaC_Expansion2.sql
@@ -0,0 +1,5 @@
+/* Gathering Storm Load-Only */
+INSERT INTO RandomAgendas_XP2
+		(AgendaType,					AgendaTag,			RequiresReligion)
+VALUES	('AGENDA_SAILOR_FLIRTATIOUS',	'AGENDA_ANY_ERA',	0),
+		('AGENDA_SAILOR_CURMUDGEON',	'AGENDA_ANY_ERA',	0);
